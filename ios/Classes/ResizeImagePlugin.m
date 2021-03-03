c696e67319ab99de48303f6a9c81999d8bf6e251
ColorfulWood <gshzyfx@163.com>
2021-03-03 18:08:08 +0800

    修改bug

--- ios/Classes/ResizeImagePlugin.m
+++ ios/Classes/ResizeImagePlugin.m
@@ -18,7 +18,7 @@
           NSDictionary* params = (NSDictionary *)call.arguments;
     //      NSLog(@"%@", params);
           NSString* imagePath = [params objectForKey:@"imagePath"];
-          NSString* url = params objectForKey:@"url"];
+          NSString* url = [params objectForKey:@"url"];
           if(imagePath == nil || url == nil){
               result(@{@"res":@"error"});
           }
@@ -27,11 +27,11 @@
           if(image == nil){
               result(@{@"res":@"error"});
           }
-        #define MAX_IMAGE_WIDTH(url) ([url containsString:@"https://cdn.lingoace.com/avatar"] ? 414 : 2480)
-        #define MAX_IMAGE_HEIGHT(url) ([url containsString:@"https://cdn.lingoace.com/avatar"] ? 414 : 2480)
+        #define MAX_IMAGE_WIDTH(url) ([url containsString:@"https://cdn.lingoace.com/avatar"] ? 414. : 2480.)
+        #define MAX_IMAGE_HEIGHT(url) ([url containsString:@"https://cdn.lingoace.com/avatar"] ? 414. : 2480.)
             if((image.size.width > MAX_IMAGE_WIDTH(url)) || (image.size.height > MAX_IMAGE_HEIGHT(url))){
               CGFloat scale = 1.0;
-              scale = MIN(MAX_IMAGE_WIDTH / image.size.width, MAX_IMAGE_HEIGHT / image.size.height) ;
+              scale = MIN(MAX_IMAGE_WIDTH(url) / image.size.width, MAX_IMAGE_HEIGHT(url) / image.size.height) ;
               CGSize size = CGSizeApplyAffineTransform(image.size, CGAffineTransformMakeScale(scale, scale));
               CGFloat scaleScreen = 0.0; // Automatically use scale factor of main screen
               UIGraphicsBeginImageContextWithOptions(size, false, scaleScreen);
